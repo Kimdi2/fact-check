4f9fe290ef73c6f27c70f4edad1ca5c173389127
bigperson <daein7076@naver.com>
2022-08-08 00:33:59 +0900

    semi change(add html)

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,26 +1,11 @@
-const pure_menu = document.querySelector("#pure");
-const pure_label = document.querySelector("#pure + label");
-const pure_content = document.querySelectorAll(".pure");
-console.log(pure_content);
+$(".content_box").click(function () {
+  $(this).children(".next_quest").toggle(300);
+});
+// const contentBox = Array.from(document.getElementsByClassName("content_box"));
 
-const action_menu = document.querySelector(".menu_list #action");
-const conflict_menu = document.querySelector(".menu_list #conflict");
-const case_law_menu = document.querySelector(".menu_list #case_law");
-console.log(pure_menu, pure_label);
-
-function changeBgColor() {
-  if (pure_menu.checked) {
-    pure_label.classList.add("changed-bg");
-    // pure_content.classList.add("hide");
-    for (i = 0; i < pure_content.length; i++) {
-      pure_content[i].classList.add("hide");
-    }
-  } else {
-    pure_label.classList.remove("changed-bg");
-    for (i = 0; i < pure_content.length; i++) {
-      pure_content[i].classList.remove("hide");
-    }
-  }
-}
-
-pure_label.addEventListener("click", changeBgColor);
+// contentBox.forEach(click);
+// function click(event) {
+//   event.children[0].addEventListener("click", (event) => {
+//     event.path[1].children[1].classList.toggle("show");
+//   });
+// }
