91b66e9b357b9885a8017fd64cd93f4e1df014d3
bigperson <daein7076@naver.com>
2022-09-11 07:24:24 +0900

    search, checkbox inproved

--- js/checkbox.js
+++ js/checkbox.js
@@ -9,3 +9,33 @@ $(".content_box").click(function () {
 //     event.path[1].children[1].classList.toggle("show");
 //   });
 // }
+function checkbox_function() {
+  const checkboxId = $(this).attr("id").split("--")[0];
+  if ($(this).attr("id") === `${checkboxId}--checkbox`) {
+    // $(`.${checkboxId}`).toggle(100);
+    const toggling = $(`.${checkboxId}`);
+    for (i = 0; i < toggling.length; i++) {
+      if (toggling[i].classList.contains("hide")) {
+        // if (toggling[i].style.display === "none") {
+        toggling[i].classList.remove("hide");
+        toggling[i].style.display = "block";
+      } else {
+        toggling[i].classList.add("hide");
+        toggling[i].style.display = "none";
+      }
+    }
+  }
+}
+
+$("input[type='checkbox']").on("click", checkbox_function);
+$("input[type='checkbox']").on("click", searcher);
+
+// function is_checked() {
+//   const checkbox_checked = $("input[type='checkbox']");
+//   const is_checked = checkbox_checked.checked;
+//   const is_checked = $(this).prop("checked");
+//   if (is_checked) {
+//     console.log($(this), is_checked);
+//   }
+// }
+// $("input[type='checkbox']").on("click", is_checked);
--- js/search.js
+++ js/search.js
@@ -1,17 +1,25 @@
+const search = document.getElementById("search");
+// 검색 입력할 시 다른 페이지 넘어가도 기존 검색어 유지
 function searcher() {
   let search = document.getElementById("search").value.toLowerCase();
   let content_box = document.getElementsByClassName("content_box");
+  console.log(search);
 
   for (let i = 0; i < content_box.length; i++) {
-    question = content_box[i].getElementsByClassName("question");
-    // answer = content_box[i].getElementsByClassName("answer");
-    if (
-      question[0].innerHTML.toLowerCase().indexOf(search) != -1 //||
-      // answer[0].innerHTML.toLowerCase().indexOf(search) != -1
-    ) {
-      content_box[i].style.display = "block";
-    } else {
-      content_box[i].style.display = "none";
+    if (!content_box[i].classList.contains("hide")) {
+      let question = content_box[i].getElementsByClassName("question");
+      if (question[0].innerHTML.toLowerCase().indexOf(search) != -1) {
+        content_box[i].style.display = "block";
+        // content_box[i].classList.remove("hide");
+      } else {
+        content_box[i].style.display = "none";
+        // content_box[i].classList.add("hide");
+      }
+      if (search === "") {
+        $(".content_box").removeAttr("style");
+      }
     }
   }
 }
+
+search.addEventListener("keyup", searcher);
